ea99cfc7909d57386bc6a92861eb442cf4b15e55
Mir Bhatia <m.bhatia@openproject.com>
2025-06-02 16:49:02 +0200

    Revert custom handling for project_list_sharing

--- app/models/sharing_strategies/project_query_strategy.rb
+++ app/models/sharing_strategies/project_query_strategy.rb
@@ -105,6 +105,14 @@ module SharingStrategies
       end
     end
 
+    def manage_shares_component(modal_content:, errors:)
+      if EnterpriseToken.allows_to?(:project_list_sharing)
+        super
+      else
+        Shares::ProjectQueries::UpsellComponent.new(modal_content:)
+      end
+    end
+
     def title
       I18n.t(:label_share_project_list)
     end
